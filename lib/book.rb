1f969e878aeb9b8d48f6e0a9f6cab55e06591412
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-03 19:38:18 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+class Book 
+  def initialize(title)
+    @title = title 
+  end
+  
+  def title 
+    @title 
+  end
+  
+  def author=(author_name)
+    @author = author_name
+  end
+  
+  def author 
+    @author 
+  end
+  
+  def page_count=(page_num)
+    @page_count = page_num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def genre=(book_genre)
+    @genre = book_genre
+  end
+  
+  def genre
+    @genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
+end
 
+Book.new("And Then There Were None")
